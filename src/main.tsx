a7d2665eafa6961d910443ecff61580d75fe0e27
Dan Harper <anok4dan@gmail.com>
2026-06-20 03:24:39 +0100

    Updated readme.md formatting

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import { createRoot } from 'react-dom/client';
 import { WagmiProvider } from 'wagmi';
 import { config } from './config/config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import '@fontsource-variable/inter/variable.css';
+import '@fontsource-variable/inter';
 import './index.css';
 import App from './App';
 
